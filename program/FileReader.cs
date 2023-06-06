a885a3133a21cc4c8be3af9932d444c3e1569b53
Владислав <@->
2023-06-06 07:23:04 +0300

    Добавьте файлы проекта.

---
+++ program/FileReader.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+using Microsoft.Office.Interop.Word;
+using Microsoft.Win32;
+using System.Windows.Xps.Packaging;
+using System.Windows.Xps.Serialization;
+using System.Windows.Documents;
+using System.Windows.Markup;
+
+namespace program
+{
+    class FileReader
+    {
+        private RichTextBox rtb;
+        private tabPanel tp;
+        public FileReader(RichTextBox _rtb, tabPanel _tp)
+        {
+            tp = _tp;
+            rtb = _rtb;
+            Init();
+        }
+        private void Init()
+        {
+            string path = Directory.GetCurrentDirectory();
+            switch (tp)
+            {
+                case tabPanel.start:
+                    {
+                        path += @"\start.rtf";
+                        break;
+                    }
+                case tabPanel.test:
+                    {
+                        path += @"\testing.rtf";
+                        break;
+                    }
+                case tabPanel.unit:
+                    {
+                        path += @"\unit.rtf";
+                        break;
+                    }
+                case tabPanel.integration:
+                    {
+                        path += @"\integration.rtf";
+                        break;
+                    }
+                case tabPanel.prepare:
+                    {
+                        path += @"\prepare.rtf";
+                        break;
+                    }
+                case tabPanel.unitTesting:
+                    {
+                        path += @"\unit-testing.rtf";
+                        break;
+                    }
+                case tabPanel.end:
+                    {
+                        path += @"\end.rtf";
+                        break;
+                    }
+            }
+
+            TextRange t = new TextRange(rtb.Document.ContentStart, rtb.Document.ContentEnd);
+            using (FileStream fs = File.Open(path, FileMode.Open))
+            {
+                if (Path.GetExtension(path).ToLower() == ".rtf")
+                {
+                    t.Load(fs, DataFormats.Rtf);
+                }
+            }
+        }
+    }
+}
---
+++ program/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace program
+{
+    public enum tabPanel
+    {
+        start,
+        test,
+        unit,
+        integration,
+        prepare,
+        unitTesting,
+        end
+    };
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            FileReader frStart = new FileReader(txtIntro, tabPanel.start);
+            FileReader frTesting = new FileReader(txtTesting, tabPanel.test);
+            FileReader frUnit = new FileReader(txtUnit, tabPanel.unit);
+            FileReader frIntegraion = new FileReader(txtIntegration, tabPanel.integration);
+            FileReader frPrepare = new FileReader(txtPrepare, tabPanel.prepare);
+            FileReader frUnitTesting = new FileReader(txtUnitTesting, tabPanel.unitTesting);
+        }
+    }
+}
